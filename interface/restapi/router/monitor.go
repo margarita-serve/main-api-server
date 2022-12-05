128d758e867f39bb501b187b017a7bdef134f32d
황종현 <goldenbell@acornsoft.io>
2022-12-05 11:20:27 +0900

    add: x-forwaded-proto header for graph server

--- interface/restapi/router/monitor.go
+++ interface/restapi/router/monitor.go
@@ -1,12 +1,14 @@
 package router
 
 import (
+	"net/http"
+	"net/url"
+
 	"git.k3.acornsoft.io/msit-auto-ml/koreserv/interface/restapi/feature"
 	internalMiddleware "git.k3.acornsoft.io/msit-auto-ml/koreserv/interface/restapi/middleware"
 	"git.k3.acornsoft.io/msit-auto-ml/koreserv/system/handler"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"net/url"
 )
 
 func SetMonitor(eg *echo.Group, h *handler.Handler, f *feature.FMonitor) {
@@ -36,8 +38,19 @@ func SetMonitor(eg *echo.Group, h *handler.Handler, f *feature.FMonitor) {
 		Rewrite: map[string]string{
 			"^/api/v1/deployments/*/monitor/graph/*": "/api/v1/deployments/graph-svr/$1/$2",
 		},
+		Transport: MyRoundTripper{r: http.DefaultTransport},
 	})
 
 	gc2 := gc.Group("/graph", ACAOHeaderOverwriteMiddleware)
 	gc2.Use(transURL)
+}
+
+type MyRoundTripper struct {
+	r http.RoundTripper
+}
+
+func (mrt MyRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Add("X-Forwarded-Proto", r.Header.Get("X-Forwarded-Proto"))
+	//fmt.Println("X-Forwarded-Proto: %s", r.Header.Get("X-Forwarded-Proto"))
+	return mrt.r.RoundTrip(r)
 }
